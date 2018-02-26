6c2f7d7e609e33398911ca7724d9b54a4e815e6e
Andre Luiz da Silva <andreh.luiz.silva@gmail.com>
2018-02-26 11:10:58 -0300

    fix(resources): Fix lambda function to start/stop RDS instances and renamed files

--- start-resources-schedule.js
+++ start-resources-schedule.js
@@ -10,4 +10,12 @@ exports.handler = function(event, context) {
             
         context.done(err,data);
     });
+
+    var rds = new AWS.RDS({region: 'us-west-1'});
+    rds.startDBInstance({DBInstanceIdentifier: 'imx-db-pre-prod-web-sql-server'}, function (err, data) {
+        if (err)
+            console.log(err, err.stack); // an error occurred  
+        else
+            console.log(data); // successful response
+    });
 };
