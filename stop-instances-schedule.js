0768153ba5ab9fa1b25d35092ec4a8e0aec70538
Andre Luiz da Silva <andreh.luiz.silva@gmail.com>
2018-02-26 10:42:03 -0300

    feat(start-stop-instances): Add lambda functions to start and stop instances based on schedule trigger

---
+++ stop-instances-schedule.js
@@ -0,0 +1,13 @@
+var AWS = require('aws-sdk');
+
+exports.handler = function(event, context) {
+    var ec2 = new AWS.EC2({region: 'us-west-1'});
+    ec2.stopInstances({InstanceIds : ['i-3135ae6b', 'i-d035158b', 'i-064fcb5fc047f13e7'] },function (err, data) {
+        if (err) 
+            console.log(err, err.stack); // an error occurred
+        else 
+            console.log(data); // successful response
+
+        context.done(err,data);
+    });
+};
